63c8b9e0fba18835a0df32d262415ac4f59d210e
Aprilianalh <61174681+Aprilianalh@users.noreply.github.com>
2020-03-10 22:23:14 +0700

    Add files via upload

---
+++ Tugas5/Tugas3.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Biodata </title>
+	<h3> FORM BIODATA </h3>
+</head>
+<body>
+  <table border=0>
+  <form action="Tugas_3.php" method="post">
+  <tr>
+   		<td> Nama Lengkap </td>
+   		<td> : </td>
+   		<td><input type=text name=Nama></td>
+  </tr>
+  
+  <tr>
+  		<td> Jurusan </td>
+  		<td> : </td>
+   		<td><input type="text" name="Jurusan"></td>
+  </tr>
+  
+  <tr>
+  		<td> Tempat dan Tanggal Lahir </td>
+   		<td> : </td>
+   		<td><input type="text" name="TTL"></td>
+  </td>
+  </tr>
+  
+  <tr>
+  		<td> Agama </td>
+  		<td> : </td>
+  		<td><input type="text" name="Agama"></td>
+  </tr>
+  <tr>
+   		<td> Jenis Kelamin </td>
+   		<td> : </td>
+   		<td><input type=radio name=JK value=Laki-laki> Laki-laki <input type=radio name=JK value=Perempuan> Perempuan </td>
+  </tr>
+  
+  <tr>
+  		<td> Hobi </td>
+  		<td> : </td>
+  		<td><input type="text" name="Hobi"></td>
+  </tr>
+
+  <tr>
+   		<td> Alamat </td>
+   		<td> : </td>
+   		<td><input type=textfield name=Alamat></td>
+  </tr>
+  <tr>
+   		<td> </td>
+   		<td> </td>
+   		<td><input type="submit" name=tambah value=Submit></td>
+  </tr>
+  </form>
+  </table>
+ </body>
+</html>
---
+++ Tugas5/Tugas_3.php
@@ -0,0 +1,19 @@
+<h3>Biodata</h3>
+<?php
+
+$nama=$_POST['Nama'];
+$jurusan=$_POST['Jurusan'];
+$tempat=$_POST['TTL'];
+$agama=$_POST['Agama'];
+$JK=$_POST['JK'];
+$hobi=$_POST['Hobi'];
+$alamat=$_POST['Alamat'];
+
+echo "Nama : $nama<br>";
+echo "Jurusan : $jurusan<br>";
+echo "Tempat dan Tanggal Lahir : $tempat<br>";
+echo "Agama : $agama<br>";
+echo "Jenis Kelamin : $JK<br>";
+echo "Hobi : $hobi<br>";
+echo "Alamat : $alamat<br>";
+?>
